f9c7ee70f91a9a47c1b709df52670039b2e49809
Dima-Karpov <dimka.carpov2013@yandex.ru>
2022-02-07 09:08:21 +0300

    routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,8 @@ import LinearProgress from '@mui/material/LinearProgress';
 import {AppRootStateType} from '../state/store';
 import {useSelector} from 'react-redux';
 import {RequestStatusType} from '../state/app-reducer';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Login} from '../features/Todolists/Login/Login';
 
 
 export const App: React.FC = () => {
@@ -20,29 +22,36 @@ export const App: React.FC = () => {
 
   return (
     <>
-      <AppBar position="static">
-        <Snackbars />
-        <Toolbar style={{justifyContent: 'space-between'}}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{mr: 2}}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-            Todolist
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-        {status === 'loading' ? <LinearProgress /> : <></>}
-      </AppBar>
-      <Container>
-        <TodolistList />
-      </Container>
-    </ >
+      <BrowserRouter>
+
+        <AppBar position="static">
+          <Snackbars />
+          <Toolbar style={{justifyContent: 'space-between'}}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{mr: 2}}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+              Todolist
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+          {status === 'loading' ? <LinearProgress /> : <></>}
+        </AppBar>
+        <Container fixed>
+            <Routes>
+              <Route path={'/'} element={<TodolistList />} />
+              <Route path={'/login'} element={<Login />} />
+            </Routes>
+        </Container>
+      </BrowserRouter>
+
+    </>
   );
 };
 
